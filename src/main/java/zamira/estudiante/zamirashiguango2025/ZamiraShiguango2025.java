4724d971ec3d4a196a0cd0d641f31d1f1c477f6b
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-12-05 14:35:02 -0500

    mal

---
+++ src/main/java/zamira/estudiante/zamirashiguango2025/ZamiraShiguango2025.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package zamira.estudiante.zamirashiguango2025;
+
+/**
+ *
+ * @author user
+ */
+public class ZamiraShiguango2025 {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
---
+++ src/main/java/zamira/estudiante/zamirashiguango2025/clase1/Producto.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zamira.estudiante.zamirashiguango2025.clase1;
+
+import zamira.estudiante.zamirashiguango2025.clase1.enumeraciones.TipoProducto;
+
+/**
+ *
+ * @author user
+ */
+public class Producto {
+    
+    private String nombre;
+    private double precio;
+    private TipoProducto tipoProducto;
+    
+    
+
+    
+    
+}
